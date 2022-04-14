c4d438f4d3890c350c87736bb251f9282a2a24ab
Hiren Patel <patelhiren.hp19@gmail.com>
2022-04-14 11:24:03 +0530

    mehod with parameter VUe

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        add() {
-            this.counter = this.counter + 1;
+        add(num) {
+            this.counter = this.counter + num;
         },
-        reduce() {
-            this.counter = this.counter - 1;
+        reduce(num) {
+            this.counter = this.counter - num;
         }
     },
 });
