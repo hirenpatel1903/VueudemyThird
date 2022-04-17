85902b1239214adcbf20d5a917cd0b568ef0e437
Hiren Patel <patelhiren.hp19@gmail.com>
2022-04-17 12:23:23 +0530

    event exploring

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
+            name: '',
         }
     },
     methods: {
+        setName(event, lastname) {
+            this.name = event.target.value + ' ' + lastname;
+        },
         add(num) {
             this.counter = this.counter + num;
         },
